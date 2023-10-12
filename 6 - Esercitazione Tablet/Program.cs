07468ec189c5ea2d69af4598364c0cfba9f615d7
Iannelli.20192 <Iannelli.20192@E2-9-PC23>
2023-10-12 10:16:32 +0200

    creazione attributi; costruttori; accessor methods

--- 6 - Esercitazione Tablet/Program.cs
+++ 6 - Esercitazione Tablet/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main()
     {
@@ -8,5 +10,48 @@
 
 class Tablet
 {
+    //  la marca, la velocità (espressa in GHz), la dimensione dello schermo (espresso in pollici) e la
+    //  durata della batteria(espressa in mAh, milliampere-ora).
 
+    private string _marca;
+    private double _velGHz;
+    private int _dimPol;
+    private int _durBatmAh;
+
+    public Tablet()
+    {
+
+    }
+
+    public Tablet(string marca, double velGHz, int dimPol, int durBatmAh)
+    {
+        _marca = marca;
+        _velGHz = velGHz;
+        _dimPol = dimPol;
+        _durBatmAh = durBatmAh;
+    }
+
+    public string Marca
+    {
+        get { return _marca; }
+        set { _marca = value; }
+    }
+
+    public double VelGHz
+    {
+        get { return _velGHz; }
+        set { _velGHz = value; }
+    }
+
+    public int DimPol
+    {
+        get { return _dimPol; }
+        set { _dimPol = value; }
+    }
+
+    public int DurBatmAh
+    {
+        get { return _durBatmAh; }
+        set { _durBatmAh = value; }
+    }
 }
